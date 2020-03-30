16a8c665ca364481153a392ee4e647f19479a324
Rafyb <rbauvin@yahoo.fr>
2020-03-30 14:44:37 +0200

    store with all pokemons

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,32 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   name: 'App',
+  created() {
+    this.getAll();
+  },
   components: {
+  },
+  methods: {
+    getAll() {  
+      let pokemonList = []   
+      for(let idx =0; idx < 808; idx++){ 
+        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${idx}/`)
+        .then((response) => {       
+            pokemonList.push({
+              id:idx,
+              data:response.data
+            });
+        }).catch(error => {
+          console.log(error);
+        });
+      }
+      this.$store.commit('setPokemons',pokemonList);
+    }
   }
+
 }
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        pokemons: [],
+    },
+    getters: {
+        getPokemons: state => state.pokemons,
+    },
+    mutations: {
+        setPokemons(state, payload) {
+            if(payload) {
+                state.pokemons = payload;
+            }
+        }
+    }
+})
